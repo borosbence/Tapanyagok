7243b876ff9384c0905e2134768ee40ffde006bc
Boros Bence <hello@bitben.hu>
2021-11-16 11:59:24 +0100

    Add project files.

---
+++ Tapanyagok.API/Models/Tapanyag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace Tapanyagok.API.Models
+{
+    [Table("tapanyag")]
+    public partial class Tapanyag
+    {
+        [Key]
+        [Column(TypeName = "int(11)")]
+        public int id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string nev { get; set; }
+        public decimal energia { get; set; }
+        public decimal feherje { get; set; }
+        public decimal zsir { get; set; }
+        public decimal szenhidrat { get; set; }
+    }
+}
---
+++ Tapanyagok.API/Models/TapanyagContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Tapanyagok.API.Models
+{
+    public partial class TapanyagContext : DbContext
+    {
+        public TapanyagContext()
+        {
+        }
+
+        public TapanyagContext(DbContextOptions<TapanyagContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Tapanyag> tapanyagok { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasCharSet("utf8mb4")
+                .UseCollation("utf8mb4_general_ci");
+
+            modelBuilder.Entity<Tapanyag>(entity =>
+            {
+                entity.Property(e => e.energia).HasPrecision(10, 1);
+
+                entity.Property(e => e.feherje).HasPrecision(10, 1);
+
+                entity.Property(e => e.szenhidrat).HasPrecision(10, 1);
+
+                entity.Property(e => e.zsir).HasPrecision(10, 1);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
