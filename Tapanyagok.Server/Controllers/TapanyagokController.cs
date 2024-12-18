a7b9c73f5696786a01fe766cb228db4d072a4cd7
Bence Boros <39780892+borosbence@users.noreply.github.com>
2024-12-18 09:21:34 +0100

    Update TapanyagokController.cs

--- Tapanyagok.Server/Controllers/TapanyagokController.cs
+++ Tapanyagok.Server/Controllers/TapanyagokController.cs
@@ -16,16 +16,18 @@ namespace Tapanyagok.Server.Controllers
             _context = context;
         }
 
+        // GET: api/Tapanyagok
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tapanyag>>> Gettapanyagok()
+        {
+            return await _context.tapanyagok.ToListAsync();
+        }
+
         // POST: api/Tapanyagok/server-side
         [HttpPost]
         [Route("server-side")]
-        public async Task<IActionResult> Post([FromForm] DTPostModel model)
+        public async Task<DTResult<Tapanyag>> Post([FromForm] DTPostModel model)
         {
-            if (model.Draw == 0)
-            {
-                return BadRequest();
-            }
-
             var query = _context.tapanyagok.AsQueryable();
 
             // Összes rekord kiszámítása
@@ -34,8 +36,10 @@ namespace Tapanyagok.Server.Controllers
             // Keresés
             string? searchKey = model?.Search?.Value;
             // Szűrt rekordok száma
-            int filteredCount = 0;
-
+            // Ha nincs keresés akkor minden rekord megjelenik,
+            // ha van akkor pedig a keresésben lévő, de nem megjelent (oldalszám szerinti) rekordokat kell visszaadni
+            int filteredRecords = totalRecords;
+            // Ha a keresési kulcsszónak van értéke, akkor indítson keresést
             if (!string.IsNullOrWhiteSpace(searchKey))
             {
                 searchKey = searchKey.Replace('.', ',');
@@ -44,15 +48,21 @@ namespace Tapanyagok.Server.Controllers
                                         x.feherje.ToString().Contains(searchKey) ||
                                         x.szenhidrat.ToString().Contains(searchKey) ||
                                         x.zsir.ToString().Contains(searchKey));
-                filteredCount = await query.CountAsync();
+                filteredRecords = await query.CountAsync();
             }
 
-            // Sorba rendezés
-            string? sortKey = model.Columns[model.Order[0].Column].Data;
-            string? sortDirection = model?.Order?[0].Dir;
+            // Rendezés
+            string? sortKey = null;
+            string? sortDirection = null;
+            if (model.Order?.Count > 0) // 3. klikk javítás
+            {
+                sortKey = model.Columns[model.Order[0].Column].Data; // oszlop neve
+                sortDirection = model.Order[0].Dir; // "asc" || "desc"
+            }
+            // Ha van a rendezési kulcsszónak értéke  
             if (!string.IsNullOrEmpty(sortKey))
             {
-                bool asc = sortDirection == "desc" ? false : true;
+                bool asc = sortDirection == "asc";
                 switch (sortKey)
                 {
                     case "nev":
@@ -79,106 +89,17 @@ namespace Tapanyagok.Server.Controllers
             query = query.Skip(model.Start).Take(model.Length);
 
             // Eredmény lekérdezése memóriába
-            List<Tapanyag> result = await query.ToListAsync();
-
-            // Szűrt elemek kiszámítása
-            // Ha nincs keresés akkor minden rekord megjelenik,
-            // ha van akkor pedig a keresésben lévő, de nem megjelent (oldalszám szerinti) rekordokat kell visszaadni
-            int filteredRecords = string.IsNullOrWhiteSpace(searchKey) ? totalRecords : filteredCount;
+            List<Tapanyag> data = await query.ToListAsync();
 
             // Válasz készítése
-            DTResult<Tapanyag> response = new()
+            DTResult<Tapanyag> result = new()
             {
                 Draw = model.Draw,
                 RecordsFiltered = filteredRecords,
                 RecordsTotal = totalRecords,
-                Data = result
+                Data = data
             };
-            return Ok(response);
-        }
-
-        // GET: api/Tapanyagok
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Tapanyag>>> Gettapanyagok()
-        {
-            return await _context.tapanyagok.ToListAsync();
-        }
-
-        // GET: api/Tapanyagok/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Tapanyag>> GetTapanyag(int id)
-        {
-            var tapanyag = await _context.tapanyagok.FindAsync(id);
-
-            if (tapanyag == null)
-            {
-                return NotFound();
-            }
-
-            return tapanyag;
-        }
-
-        // PUT: api/Tapanyagok/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTapanyag(int id, Tapanyag tapanyag)
-        {
-            if (id != tapanyag.id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(tapanyag).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TapanyagExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Tapanyagok
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Tapanyag>> PostTapanyag(Tapanyag tapanyag)
-        {
-            _context.tapanyagok.Add(tapanyag);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetTapanyag", new { id = tapanyag.id }, tapanyag);
-        }
-
-        // DELETE: api/Tapanyagok/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTapanyag(int id)
-        {
-            var tapanyag = await _context.tapanyagok.FindAsync(id);
-            if (tapanyag == null)
-            {
-                return NotFound();
-            }
-
-            _context.tapanyagok.Remove(tapanyag);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool TapanyagExists(int id)
-        {
-            return _context.tapanyagok.Any(e => e.id == id);
+            return result;
         }
     }
 }
